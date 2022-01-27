e56ac3c0954b5c6dc59a7c238a4bf17aad086de2
Ad-0271 <adnansari2001@gmail.com>
2022-01-28 01:01:19 +0530

    backend completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -30,10 +30,69 @@ const getSingleOrder = catchAsyncErrors(async (req, res, next) => {
 const myOrders = catchAsyncErrors(async (req, res, next) => {
   const orders = await Order.find({ user: req.user.id });
 
-  //   if (!orders) {
-  //     return next(new ErrorHandler("Order not found with this user", 404));
-  //   }
-
   res.status(200).json({ status: "success", orders });
 });
-module.exports = { newOrder, getSingleOrder, myOrders };
+
+const getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  orders.forEach((el) => (totalAmount += el.totalPrice));
+
+  res.status(200).json({ status: "success", totalAmount, orders });
+});
+
+const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  if (order.orderStatus == "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 404));
+  }
+
+  order.orderItems.forEach(async (el) => {
+    await updateStock(el.product, el.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+
+  if (req.body.status == "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+
+  res.status(200).json({ status: "success" });
+});
+
+const updateStock = async (id, quantity) => {
+  const product = await Product.findById(id);
+
+  product.stock -= quantity;
+
+  await product.save({ validateBeforeSave: false });
+};
+
+const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.remove();
+
+  res.status(200).json({ status: "success" });
+});
+module.exports = {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+};
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,6 +3,9 @@ const {
   newOrder,
   getSingleOrder,
   myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
 } = require("../controllers/orderController");
 
 const router = express.Router();
@@ -12,5 +15,13 @@ const { isAuthenticatedUser, isAuthorized } = require("../middlewares/auth");
 router.post("/order/new", isAuthenticatedUser, newOrder);
 router.get("/order/me", isAuthenticatedUser, myOrders);
 router.get("/order/:id", isAuthenticatedUser, getSingleOrder);
+router.get("/admin/orders", isAuthenticatedUser, isAuthorized, getAllOrders);
+router.put("/admin/order/:id", isAuthenticatedUser, isAuthorized, updateOrder);
+router.delete(
+  "/admin/order/:id",
+  isAuthenticatedUser,
+  isAuthorized,
+  deleteOrder
+);
 
 module.exports = router;
